b7f1fbddeaba6ca8a2216d4195172139ca32ae9f
TedMincham <tedmincham@gmail.com>
2023-11-02 14:47:33 +1300

    Classwork 11/02/23

--- CS103.2/CS103.2.cpp
+++ CS103.2/CS103.2.cpp
@@ -7,5 +7,4 @@
 int main()
 {
     mainMenu();
-    jsonDataFile();
 }
--- CS103.2/fileManagementFunctions.h
+++ CS103.2/fileManagementFunctions.h
@@ -4,8 +4,7 @@
 #include <fstream>
 #include "json.hpp"
 
-
-extern struct userData
+struct userData
 {
 	std::string password, userName, firstName, lastName, DOB, contactNumber, gender, email, address, vehicleRego;
 	bool isAdmin;
@@ -15,7 +14,6 @@ nlohmann::json jsonDataFile()
 {
 	std::ifstream f("./Customer_registration.json");
 	nlohmann::json userDataJson = nlohmann::json::parse(f)["userData"];
-	std::cout << userDataJson;
 	return userDataJson;
 }
 
