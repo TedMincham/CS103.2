d6a77e176bc419db5293408cda56499a67f435b6
TedMincham <tedmincham@gmail.com>
2023-11-03 15:01:47 +1300

    Class work 11/3/23

--- CS103.2/fileManagementFunctions.h
+++ CS103.2/fileManagementFunctions.h
@@ -4,7 +4,7 @@
 #include <fstream>
 #include "json.hpp"
 
-struct userData
+extern struct userData
 {
 	std::string password, userName, firstName, lastName, DOB, contactNumber, gender, email, address, vehicleRego;
 	bool isAdmin;
@@ -14,11 +14,69 @@ nlohmann::json jsonDataFile()
 {
 	std::ifstream f("./Customer_registration.json");
 	nlohmann::json userDataJson = nlohmann::json::parse(f)["userData"];
+    f.close();
 	return userDataJson;
+}
+
+void addNewUser(const userData& newUser)
+{
+    nlohmann::json data = jsonDataFile();
+
+    nlohmann::json newUserJson;
+    newUserJson["password"] = newUser.password;
+    newUserJson["userName"] = newUser.userName;
+    newUserJson["firstName"] = newUser.firstName;
+    newUserJson["lastName"] = newUser.lastName;
+    newUserJson["DOB"] = newUser.DOB;
+    newUserJson["contactNumber"] = newUser.contactNumber;
+    newUserJson["gender"] = newUser.gender;
+    newUserJson["email"] = newUser.email;
+    newUserJson["address"] = newUser.address;
+    newUserJson["vehicleRego"] = newUser.vehicleRego;
+    newUserJson["isAdmin"] = newUser.isAdmin;
+
+    data.push_back(newUserJson);
+
+    std::ofstream outFile("./Customer_registration.json");
+    outFile << std::setw(4) << data;
+    outFile.close();
 }
 
 void createData()
 {
 	nlohmann::json data = jsonDataFile();
+    std::vector<userData> users;
+
+    for (const auto& userDataJson : data)
+    {
+        userData user;
+        user.password = userDataJson["password"];
+        user.userName = userDataJson["userName"];
+        user.firstName = userDataJson["firstName"];
+        user.lastName = userDataJson["lastName"];
+        user.DOB = userDataJson["DOB"];
+        user.contactNumber = userDataJson["contactNumber"];
+        user.gender = userDataJson["gender"];
+        user.email = userDataJson["email"];
+        user.address = userDataJson["address"];
+        user.vehicleRego = userDataJson["vehicleRego"];
+        user.isAdmin = userDataJson["isAdmin"];
+        users.push_back(user);
+
+        /*std::cout << "User Data:" << std::endl;
+        std::cout << "Password: " << user.password << std::endl;
+        std::cout << "User Name: " << user.userName << std::endl;
+        std::cout << "First Name: " << user.firstName << std::endl;
+        std::cout << "Last Name: " << user.lastName << std::endl;
+        std::cout << "DOB: " << user.DOB << std::endl;
+        std::cout << "Contact Number: " << user.contactNumber << std::endl;
+        std::cout << "Gender: " << user.gender << std::endl;
+        std::cout << "Email: " << user.email << std::endl;
+        std::cout << "Address: " << user.address << std::endl;
+        std::cout << "Vehicle Registration: " << user.vehicleRego << std::endl;
+        std::cout << "Is Admin: " << user.isAdmin << std::endl;
+        std::cout << std::endl;*/
+    }
+
 }
 
--- CS103.2/menuFunctions.h
+++ CS103.2/menuFunctions.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <iomanip>
 #include <Windows.h>
+#include <string>
+#include "fileManagementFunctions.h"
 
 void login()
 {
@@ -11,7 +13,63 @@ void login()
 
 void signUp()
 {
+    userData newuser;
+    std::string password, userName, firstName, lastName, DOB, contactNumber, gender, email, address, vehicleRego;
 
+    std::cout << "Enter your desired username" << std::endl;
+    std::cin.ignore();
+    std::getline(std::cin, userName);
+    newuser.userName = userName;
+
+    std::cout << "Enter your desired password" << std::endl;
+    std::getline(std::cin, password);
+    newuser.password = password;
+
+    std::cout << "Enter your first name" << std::endl;
+    std::getline(std::cin, firstName);
+    newuser.firstName = firstName;
+
+    std::cout << "Enter your last name" << std::endl;
+    std::getline(std::cin, lastName);
+    newuser.lastName = lastName;
+
+    std::cout << "Enter your date of birth" << std::endl;
+    std::getline(std::cin, DOB);
+    newuser.DOB = DOB;
+
+    std::cout << "Enter your contact number" << std::endl;
+    std::getline(std::cin, contactNumber);
+    newuser.contactNumber = contactNumber;
+
+    std::cout << "Enter your gender" << std::endl;
+    std::cout << "Press 1 for Male, 2 for Female" << std::endl;
+    std::getline(std::cin, gender);
+    if (gender == "1")
+    {
+        gender = "Male";
+    }
+    else if (gender == "2")
+    {
+        gender = "Female";
+    }
+    newuser.gender = gender;
+
+    std::cout << "Enter your email" << std::endl;
+    std::getline(std::cin, email);
+    newuser.email = email;
+
+    std::cout << "Enter your address" << std::endl;
+    std::getline(std::cin, address);
+    newuser.address = address;
+
+    std::cout << "Enter your vehicle registration number" << std::endl;
+    std::getline(std::cin, vehicleRego);
+    newuser.vehicleRego = vehicleRego;
+
+    newuser.isAdmin = 0;
+
+    std::cout << "Sign up successful" << std::endl;
+    addNewUser(newuser);
 }
 
 void mainMenu()
